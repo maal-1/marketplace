73e63653d0d1ccbe5cf92d1f07a5e483955e7bd8
maal-1 <cvvv11144fd@m123xcvtttt.com>
2025-05-07 00:53:28 +0200

    initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/test/java/se/lexicon/marketplace_api/MarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.marketplace_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
